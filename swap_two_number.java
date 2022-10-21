84453b5c0ee0f8b42bb1fe6a193f32a22b197839
shivampandey91199 <111884088+shivampandey91199@users.noreply.github.com>
2022-10-21 20:20:54 +0530

    Add files via upload

---
+++ swap_two_number.java
@@ -0,0 +1,22 @@
+package gitproject1;
+import java.util.*;  
+
+public class swap_two_number {
+	  
+	  
+	    public static void main(String a[])   
+	    {   
+	        System.out.println("Enter the value of x and y");  
+	       Scanner sc = new Scanner(System.in); 
+	        /*Define variables*/  
+	        int x = sc.nextInt();  
+	        int y = sc.nextInt();  
+	        System.out.println("before swapping numbers: "+x +" "+ y);  
+	       /*Swapping*/  
+	        x = x + y;   
+	        y = x - y;   
+	        x = x - y;   
+	        System.out.println("After swapping: "+x +"  " + y);   
+	    }   
+	}  
+
